57ea387c902d9bd2955230927862ba102719340a
LL3RD <1959643995@qq.com>
2023-09-15 18:35:02 +0800

    First Submit

--- thirdparty/mmdetection/mmdet/datasets/__init__.py
+++ thirdparty/mmdetection/mmdet/datasets/__init__.py
@@ -19,29 +19,6 @@ from .celldet import CellDetDataset
 from .celldet_brca import CellDetDataset_BRCA
 from .celldet_brca_best import CellDetDataset_BRCA_Best
 from .cell_consep_best import CellDetDataset_CoNSeP_Best
-from .celldet_pannuke import CellDetDataset_PanNuke
 from .celldet_lizard import CellDetDataset_Lizard_Best
-from .celldet_monusec import CellDetDataset_MoNuSAC_Best
 from .coco_lizard import CocoDataset_Lizard
 from .coco_brca import CocoDataset_BRCA
-from .cellseg import CellSeg
-from .cellseg_pannuke import CellSeg_PanNuke
-from .cellseg_coco import CellSeg_COCO
-from .cellseg_pannuke_coco import CellSeg_COCO_5classes
-from .celldet_sahi import CellDetDataset_SAHI
-from .celldet_sahi_500 import CellDetDataset_SAHI_500
-
-__all__ = [
-    'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
-    'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
-    'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
-    'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
-    'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
-    'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
-    'NumClassCheckHook', 'CocoPanopticDataset', 'MultiImageMixDataset',
-    'OpenImagesDataset', 'OpenImagesChallengeDataset', 'CellDetDataset',
-    'CellDetDataset_BRCA', 'CellDetDataset_BRCA_Best', 'CellDetDataset_CoNSeP_Best',
-    'CellDetDataset_PanNuke', 'CellDetDataset_Lizard_Best', 'CellDetDataset_MoNuSAC_Best',
-    'CocoDataset_Lizard', 'CocoDataset_BRCA', 'CellSeg', 'CellSeg_PanNuke',
-    'CellSeg_COCO_5classes', 'CellSeg_COCO', 'CellDetDataset_SAHI', 'CellDetDataset_SAHI_500'
-]
